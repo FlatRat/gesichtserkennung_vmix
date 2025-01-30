414c5a9f2057d4ab3ed37d2befd69b511747a0bd
FlatRat <88405190+FlatRat@users.noreply.github.com>
2025-01-30 14:43:08 +0100

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# vMix Einstellungen
+VMIX_IP = "192.168.178.23"  # Ändere zu IP des vMix-Rechners im Netzwerk
+VMIX_PORT = 8088
+VMIX_INPUT = "1"  # Nummer oder Name der Bauchbinde in vMix
+VMIX_FIELD = "Headline"  # Name des Textfeldes in der Bauchbinde
+
+# Kamera Einstellungen
+CAMERA_ID = 0  # 0 für die erste Kamera, 1 für die zweite, usw.
+
+# Pfad für Referenzbilder
+KNOWN_FACES_DIR = "data/known_faces"
+
+# Erkennungs-Einstellungen
+DETECTION_INTERVAL = 0.5
+CONFIDENCE_THRESHOLD = 1.2
